92741a73165328ab1a821b6f1b2b7be29e176a11
Kamlesh Chovatia <kamlesh.chovatia19@gmail.com>
2022-05-04 22:25:06 +0100

    Demo12

---
+++ src/Demo11.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Demo11 {
+
+
+
+    int a;
+    int b ;
+
+    Scanner scanner = new Scanner(System.in);
+
+
+    void sum(){
+        System.out.println(a+b);
+    }
+
+   // Demo11 demo11 = new Demo11();
+    public static void main(String[] args) {
+        Demo11 demo11 = new Demo11();
+        demo11.sum();
+
+    }
+
+
+}
